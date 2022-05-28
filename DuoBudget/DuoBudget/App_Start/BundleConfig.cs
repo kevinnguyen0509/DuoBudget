1a509882c2925cb2cc9881f8a2b6256bd81d31f5
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2022-05-28 10:54:23 -0700

    Change default locations for Script and Css

--- DuoBudget/DuoBudget/App_Start/BundleConfig.cs
+++ DuoBudget/DuoBudget/App_Start/BundleConfig.cs
@@ -9,22 +9,22 @@ namespace DuoBudget
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/Default/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/Default/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/Default/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/Default/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Default/bootstrap.css",
+                      "~/Content/Default/site.css"));
         }
     }
 }
