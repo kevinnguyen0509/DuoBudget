4429dcd4b2505250c0d9b7913b97377c9fdf1f6d
kevinnguyen0509 <Kevin.Nguyen0509@gmail.com>
2022-06-11 16:50:47 -0700

    Created GetChart data and finished monthly category chart

---
+++ DuoBudget/DuoBudget/DataFatory/GetData/GetChartsData.cs
@@ -0,0 +1,62 @@
+﻿using DuoBudget.Models.BudgetModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DuoBudget.DataFatory.GetData
+{
+    public class GetChartsData
+    {
+        public PieChartModel GetMonthlyCategoriesSummaryChart(int UserID, int month, int year)
+        {
+
+            PieChartModel PieChartModel = new PieChartModel();
+
+            SqlConnection SQLConn = new SqlConnection();
+            SqlCommand SQLComm = new SqlCommand();
+            SqlDataReader SQLRec;
+
+            // Configure the ConnectionString to access the database content
+            SQLConn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["tableCon"].ConnectionString;
+            SQLConn.Open();
+
+            /*string SQL = "SELECT * FROM dbo.GeneralLiabilityClaims";*/
+            string SQL = "[dbo].[dbo.ssp_BudgetSheet_GetMonthlyCategoriesSummaryChart]";
+            SQLComm = new SqlCommand(SQL, SQLConn);
+            SQLComm.CommandType = CommandType.StoredProcedure;
+            SQLComm.Parameters.AddWithValue("@UserId", UserID);
+            SQLComm.Parameters.AddWithValue("@CurrentYear", year);
+            SQLRec = SQLComm.ExecuteReader();
+
+            try
+            {
+                if (SQLRec.Read())
+                {
+                    PieChartModel.Category = SQLRec.GetString(SQLRec.GetOrdinal("Category"));
+                    PieChartModel.PercentAmount = SQLRec.GetDecimal(SQLRec.GetOrdinal("PercentAmount"));
+                    PieChartModel.DollarAmount = SQLRec.GetDecimal(SQLRec.GetOrdinal("DollarAmount"));
+                    PieChartModel.AmountTotal = SQLRec.GetDecimal(SQLRec.GetOrdinal("AmountTotal"));
+                   
+                }
+            }
+            catch (Exception e)
+            {
+
+                PieChartModel.ReturnMessage = e.Message;
+                PieChartModel.ReturnStatus = "Failed";
+                PieChartModel.newId = -1;
+
+            }
+            finally
+            {
+                SQLRec.Close();
+                SQLConn.Close();
+            }
+
+            return PieChartModel;
+        }
+    }
+}
---
+++ DuoBudget/DuoBudget/Models/Parents/PieChartModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DuoBudget.Models.BudgetModels
+{
+    public class PieChartModel : ResultMessage
+    {
+        public string Category { get; set; }
+        public Decimal PercentAmount { get; set; }
+        public Decimal DollarAmount { get; set; }
+        public Decimal AmountTotal { get; set; }
+    }
+}
